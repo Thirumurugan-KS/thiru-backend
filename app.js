4621518a732df1a126be1082288fbf75f26df52b
Thirumurugan-KS <thiruks.dev@gmail.com>
2021-12-10 16:02:56 +0530

    commit

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express")
+const fileUpload = require("express-fileupload")
+const cors = require("cors")
+const cookieParser = require("cookie-parser")
 
 const app = express()
 
 const User = require("./Routes/userRoute")
 
 app.use("/api" , User)
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+app.use(fileUpload())
 
 module.exports = app
